342df2920f35f93137d334b8f2d99e30521c6a41
Mashdev <paullopez05@gmail.com>
2022-03-18 23:59:32 -0700

    fix directory issue

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+import { getGistData } from './Services'
 import ObjectiveSkills from "./components/ObjectiveSkills";
 import InfoHeader from "./components/InfoHeader";
-import { getGistData } from './Services'
 import WorkExperience from "./components/WorkExperience";
 import Container from "./components/Container";
 
 
 export default function App() {
-  const [getApi, setApi] = useState('')
+  const [getApi, setApi] = useState()
   const [getObjSkills, setObjSkills] = useState('')
   const [getObjBody, setObjBody] = useState('')
 
   useEffect( () => {
-    getGistData('a68a015c570a3896cb19c863a9f35e1a')
+    getGistData()
     .then(res => {
-      const newObj = 'resume.header.md';
-      const response = res.files[newObj].content
-      const contentData = response
-      setApi(contentData)
+      // const newObj = 'resume.header.md';
+      // const response = res.files[newObj].content
+      const contentData = res
+      setApi(res)
+ 
+      
     })
   }, [])
 
-  useEffect( () => {
-    getGistData('0e9fade12af7fae59f5b535d032db62a')
-    .then(res => {
-      const newObj = 'resume.obj.skills.md';
-      const response = res.files[newObj].content;
-      setObjSkills(response);
-    })
-  })
-  
-  useEffect( () => {
-    getGistData('6a7fd3ec2c19993f60bc643cee0a6ecc')
-    .then(res => {
-      const newObj = 'resume.body.md';
-      const response = res.files[newObj].content;
-      setObjBody(response);
-    })
-  })  
 
   return (
     <>
-      <InfoHeader props={getApi}></InfoHeader>
-      <ObjectiveSkills props={getObjSkills}></ObjectiveSkills>
-      <WorkExperience props={getObjBody}></WorkExperience>
+      {/* <InfoHeader props={getApi}></InfoHeader> <ObjectiveSkills props={getObjSkills}></ObjectiveSkills> <WorkExperience props={getObjBody}></WorkExperience> */}
+      <h1 className="text-3xl font-bold underline">
+        Hello Universe!
+      </h1>
+      
+      
     </>
   );
 }
--- ui/src/components/Container.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-    margin: 0 auto;
-    background-color: #e8e8e8;
-`;
-
-export default Container;
